1dee8922e29a704ffce304cb140bc311a5620e13
Gettaa <minemester150@gmail.com>
2023-01-13 09:15:37 +0100

    kamera

--- Colworld.cs
+++ Colworld.cs
@@ -2,14 +2,21 @@ using Godot;
 
 public class Colworld : Node2D
 {
+    Camera2D Kamera;
+    KinematicBody2D Jatekos;
 
-    public void onPrincessBodyEntered(PhysicsBody2D body)
-    {
-        if (body.GetName().Equals("player"))
-        {
+    public override void _Ready() {
+        Kamera = (Camera2D)GetNode("playerkamera");
+        Jatekos = (KinematicBody2D)GetNode("player");
+    }
+    public override void _Process(float delta) {
+        Kamera.Position = new Vector2(Jatekos.Position.x, Jatekos.Position.y);
+    }
+
+    public void onPrincessBodyEntered(PhysicsBody2D body) {
+        if (body.GetName().Equals("player")) {
             Label youWin = GetNode("youwin") as Label;
             youWin.Show();
         }
     }
-
 }
