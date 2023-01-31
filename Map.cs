ffeb567b479fe07dfa71e4fe80bf12cd3a55fb26
Gettaa <minemester150@gmail.com>
2023-01-31 14:01:00 +0100

    undorito haladás

--- Map.cs
+++ Map.cs
@@ -3,18 +3,28 @@ using System;
 
 public class Map : Node2D {
     Camera2D Cam;
-    Node2D Player;
+    KinematicBody2D Player;
     int anyadcounter = 0;
+    bool halott = false;
     public override void _Ready() {
         Cam = (Camera2D)GetNode("PlayerCamera");
-        Player = (Node2D)GetNode("Player/PlayerBody");
+        Player = (KinematicBody2D)GetNode("Player/PlayerBody");
     }
 
     public override void _Process(float delta) {
         Cam.Position = new Vector2(Player.Position.x + 512, Cam.Position.y);
+        if (halott && Input.IsActionPressed("respawn")) {
+            anyadcounter = 0;
+            halott = false;
+            GetTree().Paused = false;
+            GetTree().ReloadCurrentScene();
+        }
     }
-    void _Bokes(Node valami) {
-        GD.Print("anyád");
-        if (++anyadcounter > 1) GetTree().Paused = true;
+    void _Bokes(Node2D valami) {
+            GD.Print("anyá(i)d");
+        if (++anyadcounter > 1) { 
+            GetTree().Paused = true;
+            halott = true;
+        }
     }
 }
