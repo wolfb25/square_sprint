f645726a13750f08ab441e9667f5c608a9f47659
Gettaa <minemester150@gmail.com>
2023-01-31 13:30:41 +0100

    mukodo spiketilemap

--- Map.cs
+++ Map.cs
@@ -4,6 +4,7 @@ using System;
 public class Map : Node2D {
     Camera2D Cam;
     Node2D Player;
+    int anyadcounter = 0;
     public override void _Ready() {
         Cam = (Camera2D)GetNode("PlayerCamera");
         Player = (Node2D)GetNode("Player/PlayerBody");
@@ -12,8 +13,8 @@ public class Map : Node2D {
     public override void _Process(float delta) {
         Cam.Position = new Vector2(Player.Position.x + 512, Cam.Position.y);
     }
-
-    public void _Bokes() {
-        GetTree().Paused = true;
+    void _Bokes(Node valami) {
+        GD.Print("anyád");
+        if (++anyadcounter > 1) GetTree().Paused = true;
     }
 }
