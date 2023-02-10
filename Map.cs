9aca8e1deaeb13d9f1b2cbbeacc8695a95b7651c
wolfb25 <fbalint0126@gmail.com>
2023-02-10 09:31:36 +0100

    kód tisztogatás

--- Map.cs
+++ Map.cs
@@ -3,17 +3,14 @@ using System;
 
 public class Map : Node2D {
     Camera2D Cam;
-    Label Starting;
-    Label Ending;
-    Label Ponting;
-    Label PBing;
+    Label Starting, Ending, Ponting;
     Node2D Hatter;
     KinematicBody2D Player;
-    int anyadcounter = 0;
+	public static bool started = false;
+    readonly int ENDSCORE = 260;
+    int got_hit = 0;
     float score = 0;
     bool end = false;
-    // float pb = 0;
-    // string pbstring;
     public static bool halott = false;
     public override void _Ready() {
         Cam = (Camera2D)GetNode("PlayerCamera");
@@ -22,50 +19,31 @@ public class Map : Node2D {
         Ending = (Label)GetNode("EndLabel");
         Ponting = (Label)GetNode("PontLabel");
         Hatter = (Node2D)GetNode("BackgroundNode");
-        // PBing = (Label)GetNode("PBLabel");
     }
 
     public override void _Process(float delta) {
-        if (score >= 260) {
+        if (score >= ENDSCORE) {
             Cam.Position = new Vector2(Cam.Position.x, Cam.Position.y);
             Ponting.RectPosition = new Vector2(Ponting.RectPosition.x, 8);
             end = true;
-            score = 260;
+            score = ENDSCORE;
             Hatter.Position = new Vector2(Hatter.Position.x, Hatter.Position.y);
-        } 
-        else {
+        } else {
             Cam.Position = new Vector2(Player.Position.x + 512, Cam.Position.y);
             Ponting.RectPosition = new Vector2(Player.Position.x + 8, 8);
             Hatter.Position = new Vector2(Player.Position.x * 0.35f, Hatter.Position.y);
         }
         Ending.RectPosition = new Vector2(Player.Position.x - 250, Ending.RectPosition.y);
         if ((halott || end) && Input.IsActionJustPressed("respawn")) {
-            // GD.Print("apád");
-            halott = false;
-            end = false;
+            halott = false; end = false;
             GetTree().ReloadCurrentScene();
-            //Player.Position= new Vector2(168, 440);
-            anyadcounter = 1;
-            PlayerBody.started = true; 
+            got_hit = 1;
+            started = true; 
         }
-        if (PlayerBody.started && !halott) {
-            Ponting.Text = Math.Round(score/260*100).ToString() + "%";
+        if (started && !halott) {
+            Ponting.Text = Math.Round(score/ENDSCORE*100).ToString() + "%";
             score += 15 * delta;
         }
-        // GD.Print(Player.Position.x);
-       
-        // if (score > pb) {
-        //     pb = score;
-        //     pbstring = Math.Round(pb).ToString();
-        //     PBing.Text = "PB: " + pbstring;
-        // }
-        // PBing.RectPosition = new Vector2(Player.Position.x + 8, 59);
-        // if (PlayerBody.started) Starting.Visible = false; 
     }
-    void _Bokes(Node2D valami) {
-           // GD.Print("anyá(i)d");
-        if (++anyadcounter > 1) { 
-            halott = true;
-        }
-    }
+    void _Bokes(Node2D valami) { if (++got_hit > 1) halott = true; }
 }
--- PlayerBody.cs
+++ PlayerBody.cs
@@ -2,29 +2,18 @@ using Godot;
 using System;
 
 public class PlayerBody : KinematicBody2D {
-   
-    private static readonly float GRAVITY = 3000;
-    public Vector2 velocity = new Vector2(0, 0);
-    public static bool started = false;
+    Vector2 velocity = new Vector2();
+	readonly Vector2 FORCE = new Vector2(0, 3000);
     public override void _PhysicsProcess(float delta) {
-        if (!Map.halott) {
-            Vector2 force = new Vector2(0, GRAVITY);
-            velocity += force * delta;
-            velocity = MoveAndSlide(velocity, new Vector2(0, -1));
-        }
+        if (!Map.halott) velocity = MoveAndSlide(velocity += FORCE * delta, new Vector2(0, -1));
         else velocity = Vector2.Zero;
-        if (started) velocity.x = 300;
+        if (Map.started && !Map.halott && !IsOnFloor() && !IsOnWall()) Rotation += 5 * delta;
+        if (Map.started && IsOnFloor() && Rotation != 0) Rotation = 0;
+        if (Map.started) velocity.x = 300;
         if (!Map.halott && Input.IsActionPressed("space") && IsOnFloor()) {
-            started = true;
+            Map.started = true;
             velocity.y = -800;
             MoveAndSlide(velocity, Vector2.Up);
         } 
-        if (!Map.halott && started && !IsOnFloor() && !IsOnWall()) Rotation += 5 * delta;
-        if (started && IsOnFloor()) if (Rotation != 0) Rotation = 0;
     }
-	// void Spike_inside()
-	// {
-    //     GD.Print("Spike_");
-	// }
 }
-    
